b6f91540cb3d0b378d216a6f9f41d4c6cdc5cce8
Supun Pramod <pramodsupun06@gmail.com>
2025-05-16 14:33:32 +0530

    update to the code react router dom

--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import TrustPayForm from './Trustpayform.js';
+import Main from './main.js';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
+import App from './App.js';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-    <TrustPayForm />
+  <Main />
+  
   </React.StrictMode>
 );
 
